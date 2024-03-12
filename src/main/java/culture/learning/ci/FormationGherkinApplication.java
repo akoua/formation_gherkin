a9b17bc542c76ac5ca5238bb39878ea3014fe78b
akoua <akouayao225@gmail.com>
2024-03-12 08:24:52 +0000

    first_commit

---
+++ src/main/java/culture/learning/ci/FormationGherkinApplication.java
@@ -0,0 +1,13 @@
+package culture.learning.ci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationGherkinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FormationGherkinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/culture/learning/ci/FormationGherkinApplicationTests.java
@@ -0,0 +1,13 @@
+package culture.learning.ci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationGherkinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
